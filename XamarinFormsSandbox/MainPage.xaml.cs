68cc7128fe4705202e1177d1d01b3c98c90ef500
sohail-raza <44056365+sohail-raza@users.noreply.github.com>
2022-07-12 20:29:59 +0100

    Initial commit

---
+++ XamarinFormsSandbox/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinFormsSandbox.ViewModel;
+
+namespace XamarinFormsSandbox
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //bought in the view model from another class 
+            BindingContext = new MainPageEquipmentViewModel();
+        }
+
+
+    }
+}
---
+++ XamarinFormsSandbox/ViewModel/MainPageEquipmentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinFormsSandbox.ViewModel
+{
+    public class MainPageEquipmentViewModel : BindableObject
+    {
+        public MainPageEquipmentViewModel()
+        {
+            //Setting something to an Command allows me to get rid of the ugly button_clicked event handlers and pass in my own using data binding
+            IncreaseCount = new Command(OnIncrease);
+        }
+        public ICommand IncreaseCount { get; }
+
+
+        //This void is used in place of the 
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"You clicked {count} times";
+        }
+
+        int count = 0;
+        string countDisplay = "Click me";
+
+        //This is used to setup a BindingContext. BindingContexts link two properties together. I can create multiple labels with BindingConten
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay)
+                    return;
+
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
